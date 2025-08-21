3414d344cd4b4387cf5f250b65a52d3c93dcdaee
Tercio Gabriel <terciogabrieldejesus@gmail.com>
2025-08-21 20:45:35 -0300

    Docker Compose

--- getting-started/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Inicia Quarkus
-./application -Dquarkus.http.host=0.0.0.0 &
-
-# Inicia Grafana Agent
-grafana-agent -config.file=/etc/agent.yaml &
-
-# Espera os processos terminarem
-wait
